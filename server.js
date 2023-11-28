dbd7db470e8b539db8f2890cdd06e18385745456
kameyama <kameyama@na-s.jp>
2023-11-28 13:07:50 +0900

    mod: DB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { getQuery } from "https://deno.land/x/oak/helpers.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 console.log("start");
@@ -6,12 +7,77 @@ console.log("start");
 const router = new Router();
 
 router.get("/api/TList", (context) => {
-    console.log("/api/TList");
+    console.log("GET /api/TList");
     // context.response.body = "kameyamatakahito";
     const db = new DB("todo.db");
-    context.response.body = db.query("SELECT * FROM TList");
+    // let res = db.query("SELECT * FROM TList");
+    let res = db.queryEntries("SELECT id, name, done FROM TList");
+
     db.close();
+    context.response.body = res;
 });
+
+router.post("/api/TList", async (context) => {
+    console.log("POST /api/TList");
+    const params = await context.request.body({type:"form"}).value;
+
+    const db = new DB("todo.db");
+    db.query("INSERT INTO TList (name, done) VALUES (?,?)",
+    [params.get("name"), params.get("done")]);
+    db.close();
+    context.response.body = "OK";
+});
+
+router.put("/api/TList/:id", async (context) => {
+    console.log("put /api/TList");
+    const queryParams = getQuery(context, { mergeParams: true });
+    const postParams = await context.request.body({type:"form"}).value;
+    console.log(queryParams);
+    console.log(postParams);
+
+    const db = new DB("todo.db");
+    db.query("UPDATE TList SET name=?, done=? WHERE id=?",
+    [postParams.get("name"), postParams.get("done"), queryParams.id]);
+    db.close();
+    context.response.body = "OK";
+});
+
+router.delete("/api/TList/:id", async (context) => {
+    console.log("delete /api/TList");
+    const queryParams = getQuery(context, { mergeParams: true });
+    console.log(queryParams);
+
+    const db = new DB("todo.db");
+    db.query("DELETE FROM TList WHERE id=?",
+    [queryParams.id]);
+    db.close();
+    context.response.body = "OK";
+});
+
+// router.get("/api/TList1/:id", async (context) => {
+//     console.log("get1 /api/TList");
+//     const params = getQuery(context, { mergeParams: true });
+//     console.log(params);
+//     // console.log(context.params);
+//     // const params = await context.request.body({type:"form"}).value;
+
+//     // const db = new DB("todo.db");
+//     // db.query("INSERT INTO TList (name, done) VALUES (?,?)",
+//     // [params.get("name"), params.get("done")]);
+//     // db.close();
+//     context.response.body = "OK";
+// });
+// router.put("/api/TList", async (context) => {
+//     console.log("PUT /api/TList");
+//     console.log(context.params);
+//     // const params = await context.request.body({type:"form"}).value;
+
+//     // const db = new DB("todo.db");
+//     // db.query("INSERT INTO TList (name, done) VALUES (?,?)",
+//     // [params.get("name"), params.get("done")]);
+//     // db.close();
+//     context.response.body = "OK";
+// });
 
 
 
--- www/index.js
+++ www/index.js
@@ -9,16 +9,164 @@ function selectTList() {
     });
 };
 
+function postTList(name, done) {
+    return new Promise((resolve, reject) => {
+
+        let postData = "name=" + encodeURIComponent(name);
+        postData += "&done=" + encodeURIComponent(done);
+
+        let req = new XMLHttpRequest();
+        req.open("POST", "/api/TList");
+        req.onload = (event) => {
+            if (req.readyState === req.DONE) {
+                if (req.status === 200) {
+                    resolve(req.responseText);
+                } else {
+                    reject(req.responseText);
+                }
+            }
+        };
+
+        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        req.send(postData);
+    });
+};
+
+function putTList(name, done, id) {
+    return new Promise((resolve, reject) => {
+
+        let putData = "name=" + encodeURIComponent(name);
+        putData += "&done=" + encodeURIComponent(done);
+
+        let req = new XMLHttpRequest();
+        req.open("PUT", "/api/TList/" + id);
+        req.onload = (event) => {
+            if (req.readyState === req.DONE) {
+                if (req.status === 200) {
+                    resolve(req.responseText);
+                } else {
+                    reject(req.responseText);
+                }
+            }
+        };
+
+        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        req.send(putData);
+    });
+};
+
+function deleteTList(id) {
+    return new Promise((resolve, reject) => {
+
+        let req = new XMLHttpRequest();
+        req.open("DELETE", "/api/TList/" + id);
+        req.onload = (event) => {
+            if (req.readyState === req.DONE) {
+                if (req.status === 200) {
+                    resolve(req.responseText);
+                } else {
+                    reject(req.responseText);
+                }
+            }
+        };
+
+        req.send();
+    });
+};
+
+function todoInsert (id, name, done) {
+    let tdId = document.createElement("td");
+    tdId.innerHTML = id;
+    let tdName = document.createElement("td");
+    tdName.innerHTML = name;
+    let tdDone = document.createElement("td");
+    tdDone.innerHTML = done;
+
+    let tr = document.createElement("tr");
+    tr.appendChild(tdId);
+    tr.appendChild(tdName);
+    tr.appendChild(tdDone);
+
+    document.querySelector(".todolist").appendChild(tr);
+};
+
 window.onload = () => {
     // document.querySelector(".hoge").innerHTML="kameyama";
+    // let tdId = document.createElement("td");
+    // tdId.innerHTML = "101";
+    // let tdName = document.createElement("td");
+    // tdName.innerHTML = "kame";
+    // let tdDone = document.createElement("td");
+    // tdDone.innerHTML = "1";
+
+    // let tr = document.createElement("tr");
+    // tr.appendChild(tdId);
+    // tr.appendChild(tdName);
+    // tr.appendChild(tdDone);
+
+    // document.querySelector(".todolist").appendChild(tr);
+
+    // todoInsert("10", "taka", "0" );
+    // todoInsert("11", "ha", "0" );
+    // todoInsert("12", "haha", "0" );
+
+    selectTList()
+    .then ((list) => {
+        console.log(list);
+        let listObjArray = JSON.parse(list);
+        for (let i = 0; i < listObjArray.length; i++) {
+            let todo = listObjArray[i];
+            console.log(todo);
+            todoInsert(todo.id, todo.name, todo.done);
+        };
+    });
+
+    document.querySelector(".todosendbutton").addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log("todo");
+        let todoName = document.querySelector(".textname").value;
+        let radiosDone = document.querySelectorAll(".radiodone");
+        let todoDone;
+        for (let i = 0; i < radiosDone.length; i++) {
+            let r = radiosDone[i];
+            if (r.checked) {
+                todoDone = r.value;
+            }
+        }
+
+        console.log(todoDone);
+        console.log(todoName);
+
+        postTList(todoName, todoDone)
+        .then ((result) => {
+            console.log(result);
+        });
+
+    });
 
     document.querySelector(".sendbutton").addEventListener("click", (event) => {
         event.preventDefault();
         // console.log("hahahaha");
-        selectTList()
-        .then ((text) => {
-            console.log(text);
-        })
+        // selectTList()
+        // .then ((text) => {
+        //     console.log(text);
+        // })
+
+        // postTList("foo", 0)
+        // .then ((text) => {
+        //     console.log(text);
+        // });
+
+        // putTList("var", 1, 4)
+        // .then ((text) => {
+        //     console.log(text);
+        // });
+
+        // deleteTList(4)
+        // .then ((text) => {
+        //     console.log(text);
+        // });
+
     });
 
 };
